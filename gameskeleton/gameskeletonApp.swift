ddae5e47adca53ce9035a0c7df0bb6a7d63beda9
jonathanj321 <jonathan.m.jiang@gmail.com>
2025-02-26 15:37:10 -0800

    Initial Commit

---
+++ gameskeleton/gameskeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  gameskeletonApp.swift
+//  gameskeleton
+//
+//  Created by Jonathan Jiang on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct gameskeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gameskeletonTests/gameskeletonTests.swift
@@ -0,0 +1,17 @@
+//
+//  gameskeletonTests.swift
+//  gameskeletonTests
+//
+//  Created by Jonathan Jiang on 2/26/25.
+//
+
+import Testing
+@testable import gameskeleton
+
+struct gameskeletonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
