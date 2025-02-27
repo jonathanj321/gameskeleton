0eef7744ead08c32a92eacbb80f320b60521d1eb
jonathanj321 <jonathan.m.jiang@gmail.com>
2025-02-26 22:48:29 -0800

    sdfs

---
+++ DerivedData/gameskeleton/Build/Intermediates.noindex/gameskeleton.build/Debug-iphonesimulator/gameskeleton.build/Objects-normal/arm64/ContentView.1.preview-thunk.swift
@@ -0,0 +1,25 @@
+import func SwiftUI.__designTimeFloat
+import func SwiftUI.__designTimeString
+import func SwiftUI.__designTimeInteger
+import func SwiftUI.__designTimeBoolean
+
+#sourceLocation(file: "/Users/jj/Desktop/gameskeleton/gameskeleton/ContentView.swift", line: 1)
+//
+//  ContentView.swift
+//  gameskeleton
+//
+//  Created by Jonathan Jiang on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Circle()
+            .fill(.blue)
+    }
+}
+
+#Preview {
+    ContentView()
+}
