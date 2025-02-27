d915bb66bf552df80121ae168c860ff5b9be01bf
morris-francis-collins <morris.francis.collins@gmail.com>
2025-02-26 22:48:14 -0800

    test1

--- gameskeleton/ContentView.swift
+++ gameskeleton/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            var x = 1
+            Button("Hello") {
+                x += 1
+                print(x)
+            }
         }
         .padding()
     }
